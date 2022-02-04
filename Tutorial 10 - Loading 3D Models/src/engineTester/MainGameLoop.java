d8d7ad0c071f8474292d4b582deddba8bda239d2
roverdiani <87436745+roverdiani@users.noreply.github.com>
2022-02-03 23:16:31 -0300

    Add Tutorial 10

---
+++ Tutorial 10 - Loading 3D Models/src/engineTester/MainGameLoop.java
@@ -0,0 +1,45 @@
+package engineTester;
+
+import models.TexturedModel;
+import org.lwjgl.opengl.Display;
+
+import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import models.RawModel;
+import renderEngine.OBJLoader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+
+public class MainGameLoop {
+
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+
+        RawModel model = OBJLoader.loadObjModel("stall", loader);
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
+        Camera camera = new Camera();
+
+        while (!Display.isCloseRequested()) {
+            entity.increaseRotation(0, 1, 0);
+            camera.move();
+            renderer.prepare();
+            shader.start();
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+}
