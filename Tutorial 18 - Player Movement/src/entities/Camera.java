e67b36676a7d363d6c38603c2e96bea042ade590
roverdiani <87436745+roverdiani@users.noreply.github.com>
2022-02-04 19:00:59 -0300

    Add Tutorial 18

---
+++ Tutorial 18 - Player Movement/src/entities/Camera.java
@@ -0,0 +1,33 @@
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Camera {
+
+    private Vector3f position = new Vector3f(100,35,50);
+    private float pitch = 10;
+    private float yaw;
+    private float roll;
+
+    public Camera(){}
+
+    public void move() {
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public float getRoll() {
+        return roll;
+    }
+}
