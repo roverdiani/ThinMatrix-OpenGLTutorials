34ab58a760c72f4216c242bf445a5d91f45d8fb8
roverdiani <87436745+roverdiani@users.noreply.github.com>
2022-02-03 23:47:01 -0300

    Add Tutorial 11

---
+++ Tutorial 11 - Per-Pixel Lighting/src/engineTester/MainGameLoop.java
@@ -0,0 +1,50 @@
+package engineTester;
+
+import entities.Light;
+import models.TexturedModel;
+import org.lwjgl.opengl.Display;
+
+import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import models.RawModel;
+import renderEngine.OBJLoader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+
+public class MainGameLoop {
+
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
+
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -20), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
+
+        Camera camera = new Camera();
+
+        while (!Display.isCloseRequested()) {
+            entity.increaseRotation(0, 1, 0);
+            camera.move();
+            renderer.prepare();
+            shader.start();
+            shader.loadLight(light);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+}
